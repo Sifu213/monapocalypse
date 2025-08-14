3655c92991b0bd9939c14d31ebb1774dc29961b4
sifu <sifu213@gmail.com>
2025-08-14 22:51:29 +0200

    vite conf

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['@rollup/rollup-linux-x64-gnu', '@rollup/rollup-linux-x64-musl']
+    }
+  },
+  optimizeDeps: {
+    exclude: ['@rollup/rollup-linux-x64-gnu', '@rollup/rollup-linux-x64-musl']
+  }
 })
