a0d52d05019cebcd7b5e7e15d8af238d60b163b7
sifu <sifu213@gmail.com>
2025-08-15 00:04:24 +0200

    conf

--- vite.config.ts
+++ vite.config.ts
@@ -7,19 +7,11 @@ process.env.ROLLUP_NO_NATIVE = '1'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: './postcss.config.js',
-  },
   build: {
-    target: 'es2015',
-    cssCodeSplit: true,
     rollupOptions: {
       output: {
         manualChunks: undefined,
       }
     }
-  },
-  esbuild: {
-    target: 'es2015'
   }
 })
